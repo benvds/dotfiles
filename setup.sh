de8221fa5fb119d205c08e13777e876b4a19593a
Ben van de Sande <ben@pomodoro.nl>
2018-12-23 10:40:24 +0100

    update setup.sh:

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ cd ~/.dotfiles && git pull --recurse-submodules
 
 # link dotfiles
 
-ln -s ~/.dotfiles/ackrc ~/.ackrc
+# ln -s ~/.dotfiles/ackrc ~/.ackrc
 ln -s ~/.dotfiles/gemrc ~/.gemrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitignore ~/.gitignore
@@ -13,6 +13,3 @@ ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/z ~/.z
 ln -s ~/.dotfiles/ruby-version ~/.ruby-version
-
-# set shell to zsh
-chsh -s /bin/zsh
